3e536e945050ea2d35829d44ec17d55ce6fccf8e
Rob Brown <brown4homes@yahoo.com>
2019-04-29 11:19:18 -0700

    Adding stock api and services

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,7 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
+  attr_accessor :can_be_added
   
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
